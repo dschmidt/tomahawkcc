2d1084f6db1b4d3ceb62beb0fa76766713e92aee
Dominik Schmidt <domme@tomahawk-player.org>
2015-10-25 20:07:31 +0100

    Fix argv usage for uncompiled and for jxcore bundled package usage

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ fs.lstat('tomahawk/resolver.js').then(function(stats) {
 }, function () {
     throw new Error('tomahawk/resolver.js not a file, probably not in tomahawk-resolvers main dir');
 }).then(function() {
-    var args = process.argv.slice(2);
-     return compiler(args[0]);
+    var args = process.argv;
+     return compiler(args[args.length-1]);
 }).then(function(output) {
     console.log(output.code);
 }, function(err) {
